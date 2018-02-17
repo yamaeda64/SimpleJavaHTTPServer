2ffd31dc49a8bfc23cc5babe52cbf07abe03db69
yamaeda64 <joakim_bergqvist@hotmail.com>
2018-02-17 13:03:40 +0100

    added code for 403

--- SimpleJavaServer/src/tests/RequestParserTest.java
+++ SimpleJavaServer/src/tests/RequestParserTest.java
@@ -1,5 +1,6 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -20,12 +21,24 @@ public class RequestParserTest
     public final String stringGETRequestWithLongerPath = "GET /myFolder/secondFolder/image.png HTTP/1.1\n" +
             "Host: 127.0.0.1:4567\n";
     
+    public final String stringPostRequest = "POST / HTTP/1.1\n" +
+            "Host: 127.0.0.1:4567\n" +
+            "Connection: keep-alive\n" +
+            "Cache-Control: max-age=0\n" +
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36\n" +
+            "Upgrade-Insecure-Requests: 1\n" +
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" +
+            "Accept-Encoding: gzip, deflate, br\n" +
+            "Accept-Language: en-US,en;q=0.9,sv;q=0.8\n\r\n" +
+            "Name=AnInputName\n\r\n";
+            
+            
     @Test
     public void ParseRequest_TypeShouldBeGET()
     {
         RequestParser parser = new RequestParser(stringGETRequest);
-        String actual = parser.getType();
-        Assert.assertEquals("GET", actual);
+        RequestType actual = parser.getType();
+        assertEquals(RequestType.GET , actual);
     }
     
     @Test
@@ -33,7 +46,7 @@ public class RequestParserTest
     {
         RequestParser parser = new RequestParser(stringGETRequest);
         String actual = parser.getPath();
-        Assert.assertEquals("/", actual);
+        assertEquals("/", actual);
     }
     
     @Test
@@ -41,7 +54,7 @@ public class RequestParserTest
     {
         RequestParser parser = new RequestParser(stringGETRequest);
         String actual = parser.getHttpVersion();
-        Assert.assertEquals("HTTP/1.1", actual);
+        assertEquals("HTTP/1.1", actual);
     }
     
     @Test
@@ -49,8 +62,17 @@ public class RequestParserTest
     {
         RequestParser parser = new RequestParser(stringGETRequestWithLongerPath);
         String actual = parser.getPath();
-        Assert.assertEquals("/myFolder/secondFolder/image.png", actual);
+        assertEquals("/myFolder/secondFolder/image.png", actual);
     }
+    
+    @Test
+    public void ParseRequest_TypeShouldBePOST()
+    {
+        RequestParser parser = new RequestParser(stringPostRequest);
+        RequestType actual = parser.getType();
+        assertEquals(RequestType.POST, actual);
+    }
+
 }
     
 
